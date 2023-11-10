b625d97cbe66b92e25a2ec73fce4e0f42961f34c
Neelansh Jaiswal <119448065+ajneelansh@users.noreply.github.com>
2023-11-11 01:30:06 +0530

    Update animations.dart

--- animations.dart
+++ animations.dart
@@ -1,11 +1,8 @@
 class HomePage extends StatefulWidget {
-
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
