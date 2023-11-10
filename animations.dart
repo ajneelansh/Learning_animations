be4a22a5cdaae91c20b85266e1fd1c59462304af
Neelansh Jaiswal <119448065+ajneelansh@users.noreply.github.com>
2023-11-10 23:59:03 +0530

    Update animations.dart

--- animations.dart
+++ animations.dart
@@ -1,1 +1,22 @@
+class HomePage extends StatefulWidget {
 
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 120),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: Colors.indigoAccent,
+        ),
+      ),
+    );
+  }
+}
